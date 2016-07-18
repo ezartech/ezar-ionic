1dc4dd156559e5bce03368b677ee1dab3b41dc06
Wayne <na>
2016-07-18 16:47:09 -0500

    update to ezar standard MIT license

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('ionicApp', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -31,3 +31,59 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.controller("AppController", function($scope, $timeout) {
+  
+  $scope.snapshotTimestamp = Date.now();
+
+  $scope.snapshot = function() {
+          //ignore ghost clicks, wait 3 sec between invocations
+          if (Date.now() - $scope.snapshotTimestamp < 3000) return;
+          $scope.snapshotTimestamp = Date.now();
+            
+          //get snapshot & revcamera buttons to hide/show
+          var snapshotBtn = document.getElementById("snapshot");
+          var revCameraBtn = document.getElementById("revcamera");
+          
+          var inclWebView = true;    // include/exclude webView content on top of cameraView
+          var inclCameraBtns = false; // show/hide snapshot & revcamera btns
+
+          if (inclWebView && !inclCameraBtns) {
+              revCameraBtn.classList.add("hide");
+              snapshotBtn.classList.add("hide");              
+          }
+
+          setTimeout(function() {
+            ezar.snapshot(
+              function() {
+                  //perform screen capture
+                  //show snapshot button
+                  if (inclWebView && !inclCameraBtns) {
+                    snapshotBtn.classList.remove("hide");        
+                    revCameraBtn.classList.remove("hide");
+                  }
+              },null,
+              {encodingType: ezar.ImageEncoding.PNG,
+               includeWebView: inclWebView,
+               saveToPhotoAlbum: true});   
+          },0);   
+    };
+
+    $scope.reverseCamera = function() {
+      var camera = ezar.getActiveCamera();
+      if (!camera) {
+        return; //no camera running; do nothing
+      }
+
+      var newCamera = camera;
+      if (camera.getPosition() == "BACK" && ezar.hasFrontCamera()) { 
+            newCamera = ezar.getFrontCamera();
+      } else  if (camera.getPosition() == "FRONT" && ezar.hasBackCamera()) { 
+            newCamera = ezar.getBackCamera();
+      }
+
+      if (newCamera) {
+        newCamera.start();
+      }
+    }
+  });
